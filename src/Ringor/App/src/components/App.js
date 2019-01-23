10c86397cfeb047623751b8409f0c1bd87f5a071
David Lievrouw <David.lievrouw@recomatics.com>
2019-01-23 16:06:41 +0100

    Fiddle around with the landing page.

--- src/Ringor/App/src/components/App.js
+++ src/Ringor/App/src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import "./styles/styles.less";
+import "./styles/site.less";
 
 export class App extends React.Component {
   constructor(props) {
@@ -8,7 +8,19 @@ export class App extends React.Component {
 
   render() {
     return (
-      <div>Hi!</div>
+      <div className="column">
+        <h1 className="ui image header middle aligned center aligned grid">
+          <div className="four wide column"><img src="dalilogo.png" /></div>
+          <div className="two wide column">Hi!</div>
+        </h1>
+        <div className="ui stacked segment">
+          <div className="ui middle aligned center aligned grid">
+            <div className="four wide column">Swagger UI</div>
+            <div className="four wide column">Log in</div>
+            <div className="four wide column">Navigate the API</div>
+          </div>
+        </div>
+      </div>
     );
   }
 }

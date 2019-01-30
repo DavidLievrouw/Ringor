89a5c92e6a65f7d9c57d2d48f785507f54369a86
David Lievrouw <David.lievrouw@recomatics.com>
2019-01-30 17:46:36 +0100

    Finish the login page.

--- src/Ringor/App/src/components/App.js
+++ src/Ringor/App/src/components/App.js
@@ -14,28 +14,30 @@ export class App extends React.Component {
         <div class="ui huge header">
           <div class="ui massive company">{this.applicationInfo.company}</div>
         </div>
-        <div className="ui large basic label">{this.applicationInfo.product}</div>
-        <div className="ui large basic label">v{this.applicationInfo.version}</div>
-        <div className="ui large green label">
-          <i class="check circle icon"></i>
-          up and running
+        <div class="ui huge header status">
+          <div className="ui large basic label">{this.applicationInfo.product}</div>
+          <div className="ui large basic label">v{this.applicationInfo.version}</div>
+          <div className="ui large green label">
+            <i class="check circle icon"></i>
+            up and running
+        </div>
         </div>
         <div className="ui divider"></div>
-        <div className="ui three item menu">
+        <div className="ui three item secondary menu">
           <div className="item">
-            <a className="ui olive big basic image label" href="swagger">
+            <a className="ui olive huge basic image label" href="swagger">
               <img src="/swagger.png" />
               Swagger UI
             </a>
           </div>
           <div className="item">
-            <a className="ui violet big basic label" href="login">
+            <a className="ui violet huge basic label" href="login">
               <i class="sign-in icon"></i>
               Browser sign in
             </a>
           </div>
           <div className="item">
-            <a className="ui black big basic image label" href="api">
+            <a className="ui black huge basic image label" href="api">
               <img src="/api.png" />
               Navigate API
             </a>

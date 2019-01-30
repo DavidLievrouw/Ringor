abff5ebca5716cc1c77a54fa05e7d81f2a75db2f
David Lievrouw <David.lievrouw@recomatics.com>
2019-01-30 16:53:57 +0100

    Fiddle some more with the landing page.

--- src/Ringor/App/src/components/App.js
+++ src/Ringor/App/src/components/App.js
@@ -11,13 +11,33 @@ export class App extends React.Component {
       <div className="column">
         <h1 className="ui image header middle aligned center aligned grid">
           <div className="four wide column"><img src="dalilogo.png" /></div>
-          <div className="two wide column">Hi!</div>
+          <div className="eight wide column">Up-and-running!</div>
         </h1>
-        <div className="ui stacked segment">
-          <div className="ui middle aligned center aligned grid">
-            <div className="four wide column">Swagger UI</div>
-            <div className="four wide column">Log in</div>
-            <div className="four wide column">Navigate the API</div>
+        <div className="ui divider"></div>
+        <div className="ui middle aligned center aligned grid">
+          <div className="four wide column">
+            <div className="ui segment">
+              <a className="ui big image label" href="swagger">
+                <img src="/swagger.png" />
+                Swagger UI
+              </a>
+            </div>
+          </div>
+          <div className="four wide column">
+            <div className="ui segment">
+              <a className="ui big image label" href="login">
+                <i class="sign-in icon"></i>
+                Log in
+              </a>
+            </div>
+          </div>
+          <div className="four wide column">
+            <div className="ui segment">
+              <a className="ui big image label" href="api">
+                <img src="/api.png" />
+                Navigate API
+              </a>
+            </div>
           </div>
         </div>
       </div>

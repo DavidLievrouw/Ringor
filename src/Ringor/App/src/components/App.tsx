1137a5179c3302bdecebe6069d21a07da3a7b7e4
David Lievrouw <David.lievrouw@recomatics.com>
2019-02-15 08:42:15 +0100

    Split off the landing page contents into a separate component.

--- src/Ringor/App/src/components/App.tsx
+++ src/Ringor/App/src/components/App.tsx
@@ -1,59 +1,19 @@
 import * as React from "react";
-const styles = require('./styles/site.less');
-import composition from '../composition';
-import { IApplicationInfo } from '../facades/applicationInfo';
+import { Landing } from './Landing';
 
 export interface IAppProps {}
 
-export interface IAppState {
-  applicationInfo: IApplicationInfo;
-}
+export interface IAppState {}
 
 export class App extends React.Component<IAppProps, IAppState> {
-  private applicationInfo: IApplicationInfo;
 
   constructor(props: IAppProps) {
     super(props);
-    this.applicationInfo = composition.applicationInfo;
   }
 
   render() {
     return (
-      <div className="column">
-        <div className={`ui huge ${styles.header}`}>
-          <div className={`ui massive ${styles.company}`}>{this.applicationInfo.company}</div>
-        </div>
-        <div className={`ui huge ${styles.header} ${styles.status}`}>
-          <div className="ui large basic label">{this.applicationInfo.product}</div>
-          <div className="ui large green label">
-            <i className="check circle icon"></i>
-            up and running
-          </div>
-        </div>
-        <div className="ui divider"></div>
-        <div className="ui one column stackable center aligned grid">
-          <div className={`ui three item secondary menu ${styles.menu} ${styles.landing}`}>
-            <div className="item">
-              <a className="ui olive huge basic image label" href="swagger">
-                <img src="/swagger.png" />
-                Swagger UI
-              </a>
-            </div>
-            <div className="item">
-              <a className="ui violet huge basic label" href="login">
-                <i className="sign-in icon"></i>
-                Browser sign in
-              </a>
-            </div>
-            <div className="item">
-              <a className="ui black huge basic image label" href="api">
-                <img src="/api.png" />
-                Navigate API
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
+      <Landing />
     );
   }
 }
---
+++ src/Ringor/App/src/components/Landing.tsx
@@ -0,0 +1,59 @@
+import * as React from "react";
+const styles = require('./styles/site.less');
+import composition from '../composition';
+import { IApplicationInfo } from '../facades/applicationInfo';
+
+export interface ILandingProps {}
+
+export interface ILandingState {
+  applicationInfo: IApplicationInfo;
+}
+
+export class Landing extends React.Component<ILandingProps, ILandingState> {
+  private applicationInfo: IApplicationInfo;
+
+  constructor(props: ILandingProps) {
+    super(props);
+    this.applicationInfo = composition.applicationInfo;
+  }
+
+  render() {
+    return (
+      <div className="column">
+        <div className={`ui huge ${styles.header}`}>
+          <div className={`ui massive ${styles.company}`}>{this.applicationInfo.company}</div>
+        </div>
+        <div className={`ui huge ${styles.header} ${styles.status}`}>
+          <div className="ui large basic label">{this.applicationInfo.product}</div>
+          <div className="ui large green label">
+            <i className="check circle icon"></i>
+            up and running
+          </div>
+        </div>
+        <div className="ui divider"></div>
+        <div className="ui one column stackable center aligned grid">
+          <div className={`ui three item secondary menu ${styles.menu} ${styles.landing}`}>
+            <div className="item">
+              <a className="ui olive huge basic image label" href="swagger">
+                <img src="/swagger.png" />
+                Swagger UI
+              </a>
+            </div>
+            <div className="item">
+              <a className="ui violet huge basic label" href="login">
+                <i className="sign-in icon"></i>
+                Browser sign in
+              </a>
+            </div>
+            <div className="item">
+              <a className="ui black huge basic image label" href="api">
+                <img src="/api.png" />
+                Navigate API
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}

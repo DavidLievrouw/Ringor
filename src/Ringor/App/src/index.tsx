65637f3159fd951dacd1a3acc2ce1d0a2df3f17d
David Lievrouw <David.lievrouw@recomatics.com>
2019-02-22 13:36:54 +0100

    Ensure Promise support in IE11

--- src/Ringor/App/src/index.tsx
+++ src/Ringor/App/src/index.tsx
@@ -2,6 +2,8 @@ import * as React from "react";
 import {render} from 'react-dom';
 import {App} from './components/App';
 
+global.Promise = require('es6-promise').Promise; // For IE11 compatibility
+
 var appElem = document.getElementById('App');
 appElem.setAttribute("style", 'height:100%;margin:0px;');
 

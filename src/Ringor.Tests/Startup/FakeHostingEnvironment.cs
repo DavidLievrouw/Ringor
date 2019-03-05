5e6b8fa82999d7f8dc6bba55d27c674772be7cae
David Lievrouw <David.lievrouw@recomatics.com>
2019-03-05 15:08:04 +0100

    Improvements to fake hosting environment, for tests.

--- src/Ringor.Tests/Startup/FakeHostingEnvironment.cs
+++ src/Ringor.Tests/Startup/FakeHostingEnvironment.cs
@@ -1,14 +1,21 @@
 ﻿using System.IO;
+using System.Reflection;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.FileProviders;
 
 namespace Dalion.Ringor.Startup {
     public class FakeHostingEnvironment : IHostingEnvironment {
+        private static readonly Assembly MainAssembly;
+
+        static FakeHostingEnvironment() {
+            MainAssembly = typeof(Program).Assembly;
+        }
+
         public string EnvironmentName { get; set; } = Microsoft.AspNetCore.Hosting.EnvironmentName.Development;
-        public string ApplicationName { get; set; } = "Ringor";
-        public string WebRootPath { get; set; } = Path.GetDirectoryName(typeof(Program).Assembly.Location);
-        public IFileProvider WebRootFileProvider { get; set; } = new PhysicalFileProvider(Path.GetDirectoryName(typeof(Program).Assembly.Location));
-        public string ContentRootPath { get; set; } = Path.GetDirectoryName(typeof(Program).Assembly.Location);
-        public IFileProvider ContentRootFileProvider { get; set; } = new PhysicalFileProvider(Path.GetDirectoryName(typeof(Program).Assembly.Location));
+        public string ApplicationName { get; set; } = MainAssembly.GetName().Name;
+        public string WebRootPath { get; set; } = Path.GetDirectoryName(MainAssembly.Location);
+        public IFileProvider WebRootFileProvider { get; set; } = new PhysicalFileProvider(Path.GetDirectoryName(MainAssembly.Location));
+        public string ContentRootPath { get; set; } = Path.GetDirectoryName(MainAssembly.Location);
+        public IFileProvider ContentRootFileProvider { get; set; } = new PhysicalFileProvider(Path.GetDirectoryName(MainAssembly.Location));
     }
 }

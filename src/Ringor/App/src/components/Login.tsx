403234a0ad8a490e588d3880c76f7d9de148e1a3
David Lievrouw <David.lievrouw@recomatics.com>
2019-03-08 11:08:07 +0100

    Make iframe touch the borders of the div

--- src/Ringor/App/src/components/Login.tsx
+++ src/Ringor/App/src/components/Login.tsx
@@ -18,7 +18,7 @@ export class Login extends React.Component<ILoginProps, ILoginState> {
 
   render() {
     return (
-      <div className="ui middle aligned center aligned full-height bounds-adhering grid">
+      <div className={`ui middle aligned center aligned full-height bounds-adhering ${styles['padded']} grid`}>
         <div className="five wide input column">
           <h2 className="ui image header">
             <div className={`ui huge ${styles.header}`}>

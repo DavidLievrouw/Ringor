d89245d690d70bfa3d19d94f83d5b3863823fff6
David Lievrouw <David.lievrouw@recomatics.com>
2019-03-28 17:09:51 +0100

    Add cookie policy and make UI able to view it.

--- src/Ringor/App/src/components/App.tsx
+++ src/Ringor/App/src/components/App.tsx
@@ -13,7 +13,7 @@ export interface IAppProps {
   composition: IComposition
 }
 
-export interface IAppState { 
+export interface IAppState {
   hasConsent: boolean;
 }
 
@@ -26,12 +26,18 @@ export class App extends React.Component<IAppProps, IAppState> {
     }
 
     this.consentToCookieTracking = this.consentToCookieTracking.bind(this);
+    this.showCookiePolicy = this.showCookiePolicy.bind(this);
+  }
+
+  showCookiePolicy() {
+    const modal : any = $('.ui.modal');
+    modal.modal('show');
   }
 
   consentToCookieTracking() {
     const trackingConsent = this.props.composition.trackingConsent;
     document.cookie = trackingConsent.cookieString;
-    this.setState({hasConsent: true});
+    this.setState({ hasConsent: true });
   }
 
   render() {
@@ -50,12 +56,45 @@ export class App extends React.Component<IAppProps, IAppState> {
       <Router>
         <div className="app">
           <div className={`cookie nag ${this.state.hasConsent ? "hidden" : ""}`}>
-            <div className="ui floating brown message">
+            <div className="ui floating message">
               <i className="close icon" onClick={this.consentToCookieTracking}></i>
               <div className="header">
                 We use cookies
               </div>
-              <p>We use cookies to ensure you get the best experience on our website</p>
+              <p>We use cookies to give you a better service. By using this website or closing this message you agree to our <a href="#" onClick={this.showCookiePolicy}>use of cookies</a>.</p>
+              <div className="ui primary mini basic button" onClick={this.showCookiePolicy}>Learn more</div>
+            </div>
+          </div>
+          <div className="ui modal">
+            <div className="header">Cookie Policy for {applicationInfo.company} {applicationInfo.product}</div>
+            <div className="content">
+              <p><strong>What Are Cookies</strong></p>
+              <p>As is common practice with almost all professional websites this site uses cookies, which are tiny files that are downloaded to your computer, to improve your experience. This page describes what information they gather, how we use it and why we sometimes need to store these cookies. We will also share how you can prevent these cookies from being stored however this may downgrade or 'break' certain elements of the sites functionality.</p>
+              <p>For more general information on cookies see the Wikipedia article on HTTP Cookies.</p>
+              <p><strong>How We Use Cookies</strong></p>
+              <p>We use cookies for a variety of reasons detailed below. Unfortunately in most cases there are no industry standard options for disabling cookies without completely disabling the functionality and features they add to this site. It is recommended that you leave on all cookies if you are not sure whether you need them or not in case they are used to provide a service that you use.</p>
+              <p><strong>Disabling Cookies</strong></p>
+              <p>You can prevent the setting of cookies by adjusting the settings on your browser (see your browser Help for how to do this). Be aware that disabling cookies will affect the functionality of this and many other websites that you visit. Disabling cookies will usually result in also disabling certain functionality and features of the this site. Therefore it is recommended that you do not disable cookies.</p>
+              <p><strong>The Cookies We Set</strong></p>
+              <ul>
+                <li>
+                  <p>Login related cookies</p>
+                  <p>We use cookies when you are logged in so that we can remember this fact. This prevents you from having to log in every single time you visit a new page. These cookies are typically removed or cleared when you log out to ensure that you can only access restricted features and areas when logged in.</p>
+                </li>
+              </ul>
+              <p><strong>Third Party Cookies</strong></p>
+              <p>In some special cases we also use cookies provided by trusted third parties. The following section details which third party cookies you might encounter through this site.</p>
+              <ul>
+                <li>
+                  <p>From time to time we test new features and make subtle changes to the way that the site is delivered. When we are still testing new features these cookies may be used to ensure that you receive a consistent experience whilst on the site whilst ensuring we understand which optimisations our users appreciate the most.</p>
+                </li>
+              </ul>
+              <p><strong>More Information</strong></p>
+              <p>Hopefully that has clarified things for you and as was previously mentioned if there is something that you aren't sure whether you need or not it's usually safer to leave cookies enabled in case it does interact with one of the features you use on our site.</p>
+              <p>However, if you are still looking for more information, then you can contact us through one of our preferred contact methods:</p>
+              <ul>
+                <li>Email: info@dalion.eu</li>
+              </ul>
             </div>
           </div>
           <div className="ui left fixed vertical labeled icon menu fixed-width-menu">

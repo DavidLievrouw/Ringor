d2781ab8846c61e253dd9f205c8c0de0e3d9347a
David Lievrouw <David.lievrouw@recomatics.com>
2019-03-29 11:21:43 +0100

    Add some implementation to the Profile page. Doesn't look like much now...

--- src/Ringor/App/src/components/Profile.tsx
+++ src/Ringor/App/src/components/Profile.tsx
@@ -1,17 +1,26 @@
 import * as React from "react";
 import { IApplicationInfo } from '../facades/applicationInfo';
 import { ISecuredApiClient } from '../services/SecuredApiClient';
+import { Redirect } from 'react-router';
 
 export interface IProfileProps {
   applicationInfo: IApplicationInfo;
   securedApiClient: ISecuredApiClient;
 }
 
-export interface IProfileState { }
+export interface IProfileState {
+  requestsLogOut: boolean;
+}
 
 export class Profile extends React.Component<IProfileProps, IProfileState> {
   constructor(props: IProfileProps) {
     super(props);
+
+    this.state = {
+      requestsLogOut: false
+    };
+
+    this.logOut = this.logOut.bind(this);
   }
 
   componentDidMount() {
@@ -23,15 +32,45 @@ export class Profile extends React.Component<IProfileProps, IProfileState> {
       });
   }
 
+  logOut() {
+    this.setState({ requestsLogOut: true });
+  }
+
   render() {
     const user = this.props.securedApiClient.getUser();
+
+    let redirector = null;
+    if (this.state.requestsLogOut) {
+      redirector = <Redirect to="/logout" />;
+    }
+
+    let form = null;
+    if (user) {
+      form = (
+        <form className="ui form">
+          <div className="field">
+            <label>Account</label>
+            <input placeholder={user.userIdentifier} type="text" readOnly />
+          </div>
+          <div className="field">
+            <label>Name</label>
+            <input placeholder={user.name} type="text" readOnly />
+          </div>
+        </form>
+      );
+    }
+
+    let logOutButton = null;
+    if (user) {
+      logOutButton = <button className="ui button" onClick={this.logOut}>Log out</button>;
+    }
 
     return (
       <div className="ui middle aligned center aligned full-height padded padded-content grid">
         <div className="five wide input column">
-          <h2 className="content">
-            {user && user.name}
-          </h2>
+          {form}
+          {logOutButton}
+          {redirector}
         </div>
       </div>
     );

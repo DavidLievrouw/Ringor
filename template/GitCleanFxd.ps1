6020d3c6756680ca5a4ee3458d17520d81c32a01
David Lievrouw <david.lievrouw@recomatics.com>
2019-07-25 11:57:37 +0200

    Remove templating code and config. It has moved to repo DalionWebAppTemplate.

--- template/GitCleanFxd.ps1
+++
@@ -1,9 +0,0 @@
-. .\RunExe.ps1
-
-function GitCleanFxd {
-  param([string] $directory)
-  
-  $command = "git"
-  $params = "clean -fxd"
-  RunExe -command $command -params $params -workingDirectory $directory
-}
